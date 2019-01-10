4c8e6e4adb45345267de9d3f353d4462646e0448
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-10 22:26:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,8 +2,7 @@ def consolidate_cart(cart)
   consolidated_cart = {}
   cart.each do |item_hsh|
     count = cart.count(item_hsh)
-    item_hsh[1][:count] = count
-    consolidated_cart.store(item_hsh[0], item_hsh[1])
+
   end
 end
 
